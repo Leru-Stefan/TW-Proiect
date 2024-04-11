6b5631d51066d0c7fa7518ae0c51d123110a0e2f
Bejenaru Anastasia <n.bn02@icloud.com>
2024-04-11 10:09:41 +0300

    Adding problems page

--- JS files/script.js
+++ JS files/script.js
@@ -1,60 +1,7 @@
-const prev = document.querySelector("#prev");
-const next = document.querySelector("#next");
+document.addEventListener('DOMContentLoaded', function() {
+  var container = document.querySelector('.cards-frame');
+  var cards = document.querySelectorAll('.card');
+  var containerWidth = cardWidth * 3; // Afișează doar trei carduri
 
-let carouselVp = document.querySelector("#carousel-vp");
-
-let cCarouselInner = document.querySelector("#cCarousel-inner");
-let carouselInnerWidth = cCarouselInner.getBoundingClientRect().width;
-
-let leftValue = 0;
-
-// Variable used to set the carousel movement value (card's width + gap)
-const totalMovementSize =
-  parseFloat(
-    document.querySelector(".cCarousel-item").getBoundingClientRect().width,
-    10
-  ) +
-  parseFloat(
-    window.getComputedStyle(cCarouselInner).getPropertyValue("gap"),
-    10
-  );
-
-prev.addEventListener("click", () => {
-  if (!leftValue == 0) {
-    leftValue -= -totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
+  container.style.width = containerWidth + 'px';
 });
-
-next.addEventListener("click", () => {
-  const carouselVpWidth = carouselVp.getBoundingClientRect().width;
-  if (carouselInnerWidth - Math.abs(leftValue) > carouselVpWidth) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-  }
-});
-
-const mediaQuery510 = window.matchMedia("(max-width: 510px)");
-const mediaQuery770 = window.matchMedia("(max-width: 770px)");
-
-mediaQuery510.addEventListener("change", mediaManagement);
-mediaQuery770.addEventListener("change", mediaManagement);
-
-let oldViewportWidth = window.innerWidth;
-
-function mediaManagement() {
-  const newViewportWidth = window.innerWidth;
-
-  if (leftValue <= -totalMovementSize && oldViewportWidth < newViewportWidth) {
-    leftValue += totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  } else if (
-    leftValue <= -totalMovementSize &&
-    oldViewportWidth > newViewportWidth
-  ) {
-    leftValue -= totalMovementSize;
-    cCarouselInner.style.left = leftValue + "px";
-    oldViewportWidth = newViewportWidth;
-  }
-}
